078fbd2e4a0eb4ce9481eedf175153a956f68b03
Dennil_Wei <greacezli@outlook.com>
2019-03-14 13:21:20 +0800

    Initial commit

---
+++ app/src/main/java/com/gameex/dw/movietrailer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gameex.dw.movietrailer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
